b1ec9c9f47736d40c677a39742f1fa5694093d37
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-09-23 12:41:53 +0700

    Fix mixed content
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- components/organism/HTMLHead.tsx
+++ components/organism/HTMLHead.tsx
@@ -9,7 +9,7 @@ const HTMLHead = ({ title = 'Optim' }: HeaderProps) => {
 		<Head>
 			<title>{title}</title>
 			<link rel="icon" href="/favicon.ico" />
-			<link href="http://fonts.cdnfonts.com/css/graphik" rel="stylesheet"></link>
+			<link href="https://fonts.cdnfonts.com/css/graphik" rel="stylesheet"></link>
 			<link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@200;300;400;500&display=swap" rel="stylesheet"></link>
 		</Head>
 	</>
