95defc45389423897f9974f0857793c59a629d3d
James Nguyen <theuranus.ntb@gmail.com>
2021-09-24 21:05:58 +0700

    add: meta tags, og image

--- components/organism/HTMLHead.tsx
+++ components/organism/HTMLHead.tsx
@@ -5,11 +5,22 @@ interface HeaderProps {
 }
 
 const HTMLHead = ({ title = 'Optim Finance' }: HeaderProps) => {
+	const description = 'Optim is a yield aggregator for the Cardano blockchain. Optimize your DeFi yields with our suite of automated asset management products. Deposit assets in Optim’s secure vaults and our strategies take care of the rest. Boost your APYs  today. Easy, Automated, Secure.'
 	return <>
 		<Head>
 			<title>{title}</title>
 			<link rel="icon" href="/optim_fav.ico" />
-			<meta name="description" content="Optim is a yield aggregator for the Cardano blockchain. Optimize your DeFi yields with our suite of automated asset management products. Deposit assets in Optim’s secure vaults and our strategies take care of the rest. Boost your APYs  today. Easy, Automated, Secure." />
+			<meta name="title" content={title} />
+			<meta property="og:title" content={title} />
+			<meta name="description" content={description} />
+			<meta
+				property="og:description"
+				content={description}
+			/>
+			<meta name="keywords" content="optim,yield aggregator,aggregator,cardano,Cardano blockchain,defi" />
+			<meta name="copyright" content="Copyright 2021 Optim" />
+			<meta name="image" content='/assets/optim_og_image.png' />
+			<meta property="og:image" content='/assets/optim_og_image.png' />
 			<link href="https://fonts.cdnfonts.com/css/graphik" rel="stylesheet"></link>
 			<link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@200;300;400;500&display=swap" rel="stylesheet"></link>
 		</Head>
--- components/organism/Header.tsx
+++ components/organism/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
 	return (
 		<header className='my-14 px-5'>
 			<h1 className='gradient-text text-[50px] leading-[60px] font-light mb-3'>Cardano’s Yield Aggregator</h1>
-			<div className='text-base font-light mb-5'>Optim Finance is a suite of products in DeFi that is designed to generate yield on Cardano’s smart contract.</div>
+			<div className='text-base font-light mb-5'>Optim Finance is a suite of  products designed to optimize yield generation in the Cardano DeFi ecosystem. Easy, automated, secure asset management.</div>
 			<div className='bg-optim-dark rounded-sm mb-2'>
 				<input id="optim-email-input" type='text' placeholder='Enter your email'
 					className='h-12 px-5 text-white text-opacity-60 w-full bg-transparent focus-within:outline-none focus:border' />
